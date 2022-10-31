46e8fb5dc23fd8e8805914a8944e5f0c199058ac
Viktor <95305510+Lurkerrrr@users.noreply.github.com>
2022-10-31 04:05:09 +0200

    Update consumption.py

--- consumption.py
+++ consumption.py
@@ -1,6 +1,6 @@
-x = float(input("Введіть відстань автомобіля: "))
-y = float(input("Введіть споживання палива на 100 км: "))
+x = float(input("Wprowadź odległość pojazdu: "))
+y = float(input("Wpisz zużycie paliwa na 100 km: "))
 consumption = (x * y) / 100
 price = (x * y * 6.5) / 100
-print("Орієнтовна витрата палива: ", consumption)
-print("Орієнтована витрата на проїзд: ", price)
+print("Szacowane zużycie paliwa: ", consumption)
+print("Orientacyjny koszt podróży: ", price)
