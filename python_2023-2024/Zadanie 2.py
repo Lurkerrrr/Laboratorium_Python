04dc9ebc7d75ab028986b0f5ea6149f65e06d3f5
Viktor <95305510+Lurkerrrr@users.noreply.github.com>
2023-11-02 11:10:40 +0100

    Update Zadanie 2.py

--- python_2023-2024/Zadanie 2.py
+++ python_2023-2024/Zadanie 2.py
@@ -1,62 +1,62 @@
 #1), 1a)
-# def komunikat(x):
-#     print(x)
-#
-# tresc = input("Napisz dowolny komunikat: ")
-# komunikat()
+def komunikat(x):
+    print(x)
 
-# 2)
-# def func(x, y):
-#     adding = 0
-#     for i in range(x, y):
-#         print(i)
-#         adding = adding + i
-#     print(adding)
-#
-# func(5,10)
+tresc = input("Napisz dowolny komunikat: ")
+komunikat()
+
+#2)
+def func(x, y):
+    adding = 0
+     for i in range(x, y):
+         print(i)
+         adding = adding + i
+     print(adding)
+
+ func(5,10)
 
 #4)
-# def silnia(x):
-#     iloczyn = 1
-#     for i in range (1,x):
-#         print(i)
-#         iloczyn = iloczyn * i
-#     print(iloczyn)
-#
-# silnia(10)
+ def silnia(x):
+     iloczyn = 1
+     for i in range (1,x):
+         print(i)
+         iloczyn = iloczyn * i
+     
+print(iloczyn)
+silnia(10)
 
 #Napisz program, aby sprawdzić, czy liczba jest parzysta lub nieparzysta.
-# n = int(input("Podaj liczbe: "))
-#
-# def jest_parzysta(n):
-#     x = 0
-#     if int(n) % 2 == int(x):
-#         a = 'jest parzysta'
-#     else:
-#         a = 'jest nieparzysta'
-#
-#     return a
-#
-# print(jest_parzysta)
+n = int(input("Podaj liczbe: "))
+
+def jest_parzysta(n):
+    x = 0
+    if int(n) % 2 == int(x):
+        a = 'jest parzysta'
+    else:
+        a = 'jest nieparzysta'
+
+    return a
+
+print(jest_parzysta)
 
 #Calculator
-# def dodawanie(liczba1, liczba2):
-#     return liczba1 + liczba2
-# def odejmowania(liczba1, liczba2):
-#     return liczba1 - liczba2
-# def mnorzenia(liczba1, liczba2):
-#     return liczba1 * liczba2
-# def dzielenia(liczba1, liczba2):
-#     if liczba2 == 0:
-#         return None
-#     return liczba1 / liczba2
-#
-# x = {'+':dodawanie, '-':odejmowania, '*':mnorzenia, '/':dzielenia}
-# a = float(input("Podaj liczbe 1: "))
-# b = float(input("Podaj liczbe 2: "))
-# d = input("Podaj dziawania (+, -, *, /): ")
-# wynik = x[d](a,b)
-# print(wynik)
+def dodawanie(liczba1, liczba2):
+    return liczba1 + liczba2
+def odejmowania(liczba1, liczba2):
+    return liczba1 - liczba2
+def mnorzenia(liczba1, liczba2):
+    return liczba1 * liczba2
+def dzielenia(liczba1, liczba2):
+    if liczba2 == 0:
+        return None
+        return liczba1 / liczba2 
+      
+x = {'+':dodawanie, '-':odejmowania, '*':mnorzenia, '/':dzielenia}
+a = float(input("Podaj liczbe 1: "))
+b = float(input("Podaj liczbe 2: "))
+d = input("Podaj dziawania (+, -, *, /): ")
+wynik = x[d](a,b)
+print(wynik)
 
 
 
