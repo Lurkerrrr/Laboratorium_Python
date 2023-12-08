46ee0da4550ba2dbf4f32a587e047c4a48a95644
Viktor <95305510+Lurkerrrr@users.noreply.github.com>
2023-12-08 14:12:16 +0100

    Delete consumption.py

--- consumption.py
+++
@@ -1,6 +0,0 @@
-x = float(input("Wprowadź odległość pojazdu: "))
-y = float(input("Wpisz zużycie paliwa na 100 km: "))
-consumption = (x * y) / 100
-price = (x * y * 6.5) / 100
-print("Szacowane zużycie paliwa: ", consumption)
-print("Orientacyjny koszt podróży: ", price)
